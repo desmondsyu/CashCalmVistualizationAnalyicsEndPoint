00051288118fb876f9996c37edba151d98b396d3
Patric Tong <tongchakpupatrick@gmail.com>
2024-08-04 19:29:12 -0400

    Update Training

--- service/Class.py
+++ service/Class.py
@@ -6,20 +6,3 @@ from pydantic import BaseModel
 class TransType(Enum):
     IN = 1
     EX = 2
-
-
-class Transaction(BaseModel):
-    user_id: int
-    Amount: float
-    Group: str
-    Type: TransType
-
-
-class User(BaseModel):
-    user_id: int
-    username: str
-    password: str
-    email: str
-
-class CurrentUser(BaseModel):
-    user: User

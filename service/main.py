fd00509d3920ae85dbce4a50ac0964cdf2058170
Patric Tong <tongchakpupatrick@gmail.com>
2024-08-07 21:10:51 -0400

    Update middleware CROS for endpoint

--- service/main.py
+++ service/main.py
@@ -14,15 +14,15 @@ app = FastAPI(dependencies=[Depends(auth_username)])
 
 # Configure CORS settings
 origins = [
-    "http://localhost:3000"
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
